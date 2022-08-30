71437d0c3f0a98c04d4a974cd58e998dd7eeb9b1
matthew-woodington <smwoodington@gmail.com>
2022-08-30 14:04:38 -0400

    adds formatting to js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-const image = document.querySelector('.pug-image');
-const btn = document.querySelector('.swap-image');
+(function () {
+  const image = document.querySelector(".pug-image");
+  const btn = document.querySelector(".swap-image");
 
-btn.addEventListener('click', changeImage);
+  btn.addEventListener("click", changeImage);
 
-function changeImage() {
-  if (image.src.match("../awesomely-ugly-website/images/sad_pug.jpg")) {
-    image.src = "../awesomely-ugly-website/images/sad_pug2.jpg";
-  } else {
-    image.src = "../awesomely-ugly-website/images/sad_pug.jpg";
+  function changeImage() {
+    if (image.src.match("../awesomely-ugly-website/images/sad_pug.jpg")) {
+      image.src = "../awesomely-ugly-website/images/sad_pug2.jpg";
+    } else {
+      image.src = "../awesomely-ugly-website/images/sad_pug.jpg";
+    }
   }
-}
+})();
